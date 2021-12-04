a0c192bdfc84047a9227f57e0ee023ab23d91fba
ercumentlacin <ercument.lacin@akinon.com>
2021-12-04 12:09:41 +0300

    add helpers

---
+++ src/helper/index.js
@@ -0,0 +1,40 @@
+/* eslint-disable import/prefer-default-export */
+export const emailToNickname = (email) => {
+  if (!email) return '';
+  return email.split('@')[0].replace(/\W/g, '_');
+};
+
+export const timeDifference = (previous) => {
+  const msPerMinute = 60 * 1000;
+  const msPerHour = msPerMinute * 60;
+  const msPerDay = msPerHour * 24;
+  const msPerMonth = msPerDay * 30;
+  const msPerYear = msPerDay * 365;
+
+  const currentTime = Date.now();
+  const pre = new Date(previous.seconds * 1000).getTime();
+
+  const elapsed = currentTime - pre;
+
+  if (elapsed < msPerMinute) {
+    return `${Math.round(elapsed / 1000)} seconds ago`;
+  }
+
+  if (elapsed < msPerHour) {
+    return `${Math.round(elapsed / msPerMinute)} minutes ago`;
+  }
+
+  if (elapsed < msPerDay) {
+    return `${Math.round(elapsed / msPerHour)} hours ago`;
+  }
+
+  if (elapsed < msPerMonth) {
+    return `${Math.round(elapsed / msPerDay)} days ago`;
+  }
+
+  if (elapsed < msPerYear) {
+    return `${Math.round(elapsed / msPerMonth)} months ago`;
+  }
+
+  return `${Math.round(elapsed / msPerYear)} years ago`;
+};
